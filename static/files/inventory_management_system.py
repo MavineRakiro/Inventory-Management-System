b21d3839fd03d5903444a28471e37bc54ac3a6ec
Mavine Rakiro <65388647+MavineRakiro@users.noreply.github.com>
2022-05-12 22:32:29 +0300

    Add files via upload

---
+++ static/files/inventory_management_system.py
@@ -0,0 +1,11 @@
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import pickle
+
+#we are loading the model using pickle
+model = pickle.load(open('inventory_management_system', 'rb'))
+
+def predict_inventory(df):
+    predictions=model.predict(df)
+    df['predictions']=predictions
+    return(df)
